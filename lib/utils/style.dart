0216982e5c81275eb2e6cd33edbabff0f2824573
Ashiqu Ali <ashiquali007@gmail.com>
2024-03-02 11:43:15 +0530

    font changed to poppins

--- lib/utils/style.dart
+++ lib/utils/style.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 //Colors
 Color background = const Color(0xFFF9F7F3);
@@ -7,21 +8,21 @@ Color primary2 =const Color(0xFF6380fb);
 Color white = const Color(0xFFFFFFFF);
 
 //TextStyle
-TextStyle heading1 = TextStyle(
+TextStyle heading1 = GoogleFonts.poppins(
     color: white,
     fontSize: 24,
     fontWeight: FontWeight.bold
 );
-TextStyle heading2 = TextStyle(
+TextStyle heading2 = GoogleFonts.poppins(
     color: white,
     fontSize: 18,
     fontWeight: FontWeight.w500
 );
-TextStyle heading3 = const TextStyle(
+TextStyle heading3 = GoogleFonts.poppins(
     color: Colors.blue,
     fontSize: 17.5
 );
-TextStyle button = TextStyle(
+TextStyle button = GoogleFonts.poppins(
     color: white,
     fontSize: 18,
     fontStyle: FontStyle.normal
