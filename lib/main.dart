da8f34bd624b958867dc1469d79774448f05a4af
Ashiqu Ali <ashiquali007@gmail.com>
2024-03-02 11:19:43 +0530

    push

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:genie/pages/signin_page.dart';
+
+main(){
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'genie',
+      debugShowCheckedModeBanner: false,
+      home: SignInPage(),
+    );
+  }
+}
---
+++ lib/utils/style.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+//Colors
+Color background = const Color(0xFFF9F7F3);
+Color primary1 = const Color(0xFF7f30fe);
+Color primary2 =const Color(0xFF6380fb);
+Color white = const Color(0xFFFFFFFF);
+
+//TextStyle
+TextStyle heading1 = TextStyle(
+    color: white,
+    fontSize: 24,
+    fontWeight: FontWeight.bold
+);
+TextStyle heading2 = TextStyle(
+    color: white,
+    fontSize: 18,
+    fontWeight: FontWeight.w500
+);
+TextStyle heading3 = const TextStyle(
+    color: Colors.blue,
+    fontSize: 17.5
+);
+TextStyle button = TextStyle(
+    color: white,
+    fontSize: 18,
+    fontStyle: FontStyle.normal
+);
+
+//sizes
+double xsmall = 10.0;
+double small = 15.0;
+double medium = 20.0;
+double large = 30.0;
