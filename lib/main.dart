5b369fdb1a7e498583cf8b80b30fca0bf7f970aa
Ashiqu Ali <ashiquali007@gmail.com>
2024-03-16 17:06:31 +0530

    user login is checked and redirect page accordingly

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,48 @@
 import 'package:flutter/material.dart';
-import 'package:genie/pages/signup_page.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'firebase_options.dart';
+import 'pages/signup_page.dart';
+import 'pages/home_page.dart';
 
-main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  print("main completed");
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'genie',
-      debugShowCheckedModeBanner: false,
-      home: SignUpPage(),
+    return FutureBuilder(
+      // Check if the user is already signed in
+      future: FirebaseAuth.instance.authStateChanges().first,
+      builder: (context, AsyncSnapshot<User?> snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          // Firebase is still initializing, show a loading indicator
+          return const MaterialApp(
+            title: 'genie',
+            debugShowCheckedModeBanner: false,
+            home: Scaffold(
+              body: Center(
+                child: CircularProgressIndicator(),
+              ),
+            ),
+          );
+        } else {
+          // If user is signed in, navigate to home page
+          // Otherwise, navigate to signup page
+          return MaterialApp(
+            title: 'genie',
+            debugShowCheckedModeBanner: false,
+            home: snapshot.hasData ? const HomePage() : const SignUpPage(),
+          );
+        }
+      },
     );
   }
 }
